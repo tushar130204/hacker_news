8bb0f417c858eb8e34f4797240c0b75a0f0a1a0e
tushar130204 <tushar.m.sugur@gmail.com>
2025-03-28 10:57:29 +0530

    Add main routing setup with health check and integration of user, post, like, comment, and authentication routes

---
+++ src/routes/routes.ts
@@ -0,0 +1,21 @@
+import { Hono } from "hono";
+import { authenticationRoutes } from "./authentication-routes";
+import { usersRoutes } from "./users-routes";
+import { postsRoutes } from "./posts-routes";
+import { likesRoutes } from "./likes-routes";
+import { commentsRoutes } from "./comments-routes";
+import { logger } from "hono/logger";
+
+export const allRoutes = new Hono();
+
+allRoutes.use(logger());
+
+allRoutes.route("/auth", authenticationRoutes);
+allRoutes.route("/users", usersRoutes);
+allRoutes.route("/posts", postsRoutes);
+allRoutes.route("/likes", likesRoutes);
+allRoutes.route("/comments", commentsRoutes);
+
+allRoutes.get("/health", (context) => {
+  return context.json({ message: "All Ok" }, 200);
+});
